9247e5fcf02f10222ec35a363ec122d5d30a008b
behnam <behnam.mirzaeee@gmail.com>
2019-02-18 21:20:40 +0330

    craete flowing section

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,5 +1,5 @@
 import React,{Component} from "react";
-import {View,Text,StyleSheet,Platform} from "react-native";
+import {View,Text,StyleSheet,Platform,Button,Image,TouchableOpacity} from "react-native";
 import Icon from "react-native-vector-icons/Ionicons"
 import EStyleSheet from "react-native-extended-stylesheet";
 
@@ -8,26 +8,55 @@ export default class Profile extends Component {
     static navigationOptions = () => ({
         headerLeft :
             <View style={styles.headerContainer}>
-                <Icon style={styles.headerIcons} name="md-more" size={30} color="black" />
+                <Icon style={styles.headerIcons} name="md-more" size={30} color="black"/>
                 <Icon style={styles.headerIcons} name="md-person-add" size={30} color="black" />
                 <Icon style={styles.headerIcons} name="md-filing" size={30} color="black" />
             </View>,
-        headerRight : <Text style={styles.title}>بهنام میرزایی</Text>
+        headerRight : <Text style={styles.headerTitle}>بهنام میرزایی</Text>
     });
 
     render() {
         return (
-            <View>
-                <Text >
-                    Profile hjhjh Screen
-                </Text>
+            <View style={styles.container}>
+                <View style={styles.FollowAndImageSection}>
+                    <View style={styles.imageContainer}>
+                        <Image style={styles.image} source={require("./../../assets/images/profile.png")} />
+                        <Icon style={styles.imageIcon} name="md-add-circle" size={30} color="black" />
+                    </View>
+                    <View style={styles.containerInfo}>
+                        <View style={styles.profileInfo}>
+                            <View style={styles.profileInfoTextNumber}>
+                                <Text style={styles.profileInfoNumber}>129</Text>
+                                <Text style={styles.profileInfoText}>پست ها</Text>
+                            </View>
+                            <View style={styles.profileInfoTextNumber}>
+                                <Text style={styles.profileInfoNumber}>129</Text>
+                                <Text style={styles.profileInfoText}>پست ها</Text>
+                            </View>
+                            <View style={styles.profileInfoTextNumber}>
+                                <Text style={styles.profileInfoNumber}>129</Text>
+                                <Text style={styles.profileInfoText}>پست ها</Text>
+                            </View>
+                        </View>
+                        <View style={{ height : 60 , alignItems: 'center' , flexDirection : 'row' , marginLeft : 20 , marginRight : 20, marginTop : 0, paddingTop: 0 , marginBottom:20}}>
+                            <TouchableOpacity onPress={() => this.props.navigation.navigate('Settings')} activeOpacity={.6} style={{ borderWidth: 1 , borderColor : 'rgba(0,0,0,0.3)' , borderRadius: 2 , flex : 1}}>
+                                <Text style={{ textAlign : 'center' , fontFamily : 'IRANSansMobile', padding : 2}}>ویرایش پروفایل</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+                <View>
+                    <Text >
+                        Profile hjhjh Screen
+                    </Text>
+                </View>
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    title : {
+    headerTitle : {
         ...Platform.select({
             ios: {
                 fontFamily : "IRANSansMobile",
@@ -51,5 +80,91 @@ const styles = StyleSheet.create({
         marginLeft : 15,
         marginRight: 10,
         color : "rgba(0,0,0,.8)",
-    }
+        backgroundColor: "white"
+    },
+
+    container : {
+        flex : 1,
+    },
+
+    FollowAndImageSection : {
+        flexDirection: 'row-reverse' ,
+        height : 160 ,
+        paddingTop : 10,
+    },
+
+    //profile image
+    imageContainer : {
+        flex: .3,
+        alignItems : 'center',
+        marginTop : 15,
+    },
+    imageIcon : {
+        bottom : 35,
+        right : 30,
+        borderRadius: 15,
+        overflow : 'hidden',
+        color : "rgba(0,0,0,.8)",
+        backgroundColor: "white",
+        padding : 0,
+        margin : 0
+    },
+    image : {
+        width: 80,
+        height: 80,
+        borderRadius : 40,
+        borderColor : '#888',
+        borderWidth : 2,
+        margin : 5
+    },
+    profileImage : {
+        flex : .3,
+    },
+
+    //profile data followers or following
+    containerInfo : {
+        flex : 0.7,
+        flexDirection : "column",
+        marginTop : 20,
+        marginRight : 18,
+        marginLeft : 18,
+
+    },
+    profileInfo : {
+        height : 70,
+        flexDirection : 'row',
+        flex : 4,
+        justifyContent: 'space-around',
+        marginLeft : 19,
+        marginRight : 19
+    },
+    profileInfoTextNumber : {
+        flex : 0.33,
+    },
+    profileInfoText : {
+        ...Platform.select({
+            ios: {
+                fontFamily : "IRANSansMobile",
+                fontWeight : "bold",
+            },
+            android: {
+                fontFamily : "IRANSansMobile_Bold",
+            },
+        }),
+        textAlign : 'center'
+    },
+    profileInfoNumber : {
+        ...Platform.select({
+            ios: {
+                fontFamily : "IRANSansMobile",
+                fontWeight : "bold",
+            },
+            android: {
+                fontFamily : "IRANSansMobile_Bold",
+            },
+        }),
+        fontSize : 16,
+        color : '#222',
+        textAlign: 'center'
+    },
 });
